f0bfb6099d24aaace5d61d084e81cf4f8409350b
LEVIS <levisrabah@gmail.com>
2024-04-28 10:34:38 +0300

    Completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
+import Header from "./Header";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasks, setTasks] = useState(TASKS);
+  const [category, setCategory] = useState("All");
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  function handleDarkModeClick() {
+    setIsDarkMode((isDarkMode) => !isDarkMode);
+  }
+
+  const filteredTasks =
+    category === "All" ? tasks : tasks.filter((task) => task.category === category);
+
+  const handleDeleteTask = (taskIndex) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(taskIndex, 1);
+    setTasks(updatedTasks);
+  };
+
+  const handleFormSubmit = (newTask) => {
+    setTasks([...tasks, newTask]);
+  };
+
   return (
-    <div className="App">
+    <div className={`App ${isDarkMode ? "dark" : "light"}`}>
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
+      <CategoryFilter
+        categories={CATEGORIES}
+        selectedCategory={category}
+        onSelectCategory={setCategory}
+      />
+      <NewTaskForm categories={CATEGORIES} onTaskFormSubmit={handleFormSubmit} />
+      <TaskList tasks={filteredTasks} onDeleteTask={handleDeleteTask} />
     </div>
   );
 }
